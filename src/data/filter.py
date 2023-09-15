8ff5f29a28c4481b98673d47f98c878ae89028c7
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-09-15 12:14:42 +0300

    Update compute_sentence_statistics to use it in analyzer

--- src/data/filter.py
+++ src/data/filter.py
@@ -13,6 +13,7 @@ from datasets import load_dataset
 from pandarallel import pandarallel
 from tqdm import tqdm
 from typing import Tuple, List, Union
+from sklearn.model_selection import train_test_split
 
 tqdm.pandas()
 
@@ -30,7 +31,7 @@ def score_sentence(sentence: str) -> float:
     tokenized_sentence = " ".join(tokens)
     return model.score(tokenized_sentence, bos=True, eos=True)
 
-def compute_sentence_statistics(text: str, word_score_threshold: float = -3.87407) -> Tuple[List[str], int, float, float, float, List[float], List[int], List[int]]:
+def compute_sentence_statistics(text: str, word_score_threshold: float = -3.87407, return_counts=False) -> Tuple[List[str], int, float, float, float, List[float], List[int], List[int]]:
     """
     Computes various statistics about sentences in the text.
 
@@ -44,14 +45,21 @@ def compute_sentence_statistics(text: str, word_score_threshold: float = -3.8740
 
     sentences = extractor.from_paragraph(text)
     num_sentences = len(sentences)
+    num_words = len([t for t in z_tokenizer.tokenize(text) if t.content.strip() not in string.punctuation])
+
     if num_sentences == 0:
-        return [], 0, 0, 0, 0, [], [], []
+        if return_counts:
+            return [], 0, 0, 0, 0, 0, [], [], []
+        else:
+            return [], 0, 0, 0, 0, 0
 
     avg_sentence_length_char = sum(len(sentence) for sentence in sentences) / num_sentences
     sentence_lengths_word = [len(z_tokenizer.tokenize(sentence)) for sentence in sentences]
     avg_sentence_length_word = sum(sentence_lengths_word) / num_sentences
+    if not return_counts:
+        return sentences, num_sentences, num_words, avg_sentence_length_char, avg_sentence_length_word, np.std(sentence_lengths_word)
+
     scores = [score_sentence(sentence) for sentence in sentences]
-    
     weird_word_counts = []
     word_counts = []
     for sentence in sentences:
@@ -61,7 +69,8 @@ def compute_sentence_statistics(text: str, word_score_threshold: float = -3.8740
         weird_word_counts.append(len([s for s in word_scores if s < word_score_threshold]))
         word_counts.append(len(words))
     
-    return sentences, num_sentences, avg_sentence_length_char, avg_sentence_length_word, np.std(sentence_lengths_word), scores, word_counts, weird_word_counts
+    return sentences, num_sentences, num_words, avg_sentence_length_char, avg_sentence_length_word, np.std(sentence_lengths_word), scores, word_counts, weird_word_counts
+
 
 def process_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -77,10 +86,10 @@ def process_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     df['type'] = df['type'].apply(lambda x: x.strip().lower())
     df['pub_name'] = df['pub_name'].apply(lambda x: x.strip().lower())
 
-    computed_cols = df['content'].parallel_apply(lambda t: compute_sentence_statistics(t))
-    df[["sentences", "num_sentences", "avg_sentence_length_char", "avg_sentence_length_word", "std_sentence_length_word", "scores", "token_counts", "weird_token_counts"]] = pd.DataFrame(computed_cols.tolist(), index=df.index)
+    computed_cols = df['content'].parallel_apply(lambda t: compute_sentence_statistics(t, return_counts=True))
+    df[["sentences", "num_sentences", "num_words", "avg_sentence_length_char", "avg_sentence_length_word", "std_sentence_length_word", "scores", "token_counts", "weird_token_counts"]] = pd.DataFrame(computed_cols.tolist(), index=df.index)
 
-    output_df = df[["id", "date", "pub_name", "type", "title", "phase", "Label", "content", "sentences", "num_sentences", "avg_sentence_length_char", "avg_sentence_length_word", "std_sentence_length_word", "scores", "token_counts", "weird_token_counts"]]
+    output_df = df[["id", "date", "pub_name", "type", "title", "phase", "Label", "content", "sentences", "num_sentences", "num_words", "avg_sentence_length_char", "avg_sentence_length_word", "std_sentence_length_word", "scores", "token_counts", "weird_token_counts"]]
     output_df['tmp'] = output_df.apply(lambda row: list(zip(row["sentences"], row["scores"], row["token_counts"], row["weird_token_counts"])), axis=1) 
     output_df = output_df.explode("tmp")
     output_df[["sentences", "scores", "token_counts", "weird_token_counts"]] = pd.DataFrame(output_df["tmp"].tolist(), index=output_df.index)
@@ -108,8 +117,30 @@ def aggregate_and_filter_data(df: pd.DataFrame) -> pd.DataFrame:
     output_df.drop_duplicates(['id'], inplace=True)
     return output_df
 
-def save_data(df: pd.DataFrame):
-    df.to_csv('data/raw_data_v4_agg.csv')
+
+def generate_sentence_scores(sentences: List[str]) -> dict:
+    sentence_scores = {}
+    for sent in tqdm(sentences):
+        sentence_scores[sent] = score_sentence(sent)
+    return sentence_scores
+
+def generate_word_scores(sentences: List[str]) -> dict:
+    word_scores = {}
+    for sent in sentences:
+        sent = sent.translate(str.maketrans('', '', string.punctuation))
+        words = z_tokenizer.tokenize(sent)
+        for word in words:
+            text = word.content
+            if text not in word_scores:
+                word_scores[text] = score_sentence(text)
+    return word_scores
+
+def main():
+    pandarallel.initialize(progress_bar=True)
+    dataset = load_dataset("parquet", data_files={'train': 'data/data_cleaned_sentences_phases_rules.parquet'})
+    df = dataset['train'].to_pandas()
+    processed_df = process_dataframe(df)
+    aggregated_df = aggregate_and_filter_data(processed_df)
 
     filter_1 = (
         (df['has_weird_sentences_sum'] == 0) &
@@ -129,45 +160,16 @@ def save_data(df: pd.DataFrame):
         (df['scores_std'] < 0.2)
     )
 
-    df[filter_1].to_csv('data/raw_data_v4_agg_filtered_v1.csv', index=False)
-    df[filter_2].to_csv('data/raw_data_v4_agg_filtered_v2.csv', index=False)
-
-def load_sample_data(fraction: float = 0.01) -> pd.DataFrame:
-    dataset = load_dataset("parquet", data_files={'train': 'data/data_cleaned_sentences_phases_rules.parquet'})
-    df = dataset['train'].to_pandas()
-    return df.sample(frac=fraction)
-
-def generate_sentence_scores(sentences: List[str]) -> dict:
-    sentence_scores = {}
-    for sent in tqdm(sentences):
-        sentence_scores[sent] = score_sentence(sent)
-    return sentence_scores
-
-def generate_word_scores(sentences: List[str]) -> dict:
-    word_scores = {}
-    for sent in sentences:
-        sent = sent.translate(str.maketrans('', '', string.punctuation))
-        words = z_tokenizer.tokenize(sent)
-        for word in words:
-            text = word.content
-            if text not in word_scores:
-                word_scores[text] = score_sentence(text)
-    return word_scores
-
-
-def save_dataframe_to_csv(df: pd.DataFrame, path: str) -> None:
-    df.to_csv(path, index=False)
-
-def main():
-    pandarallel.initialize(progress_bar=True)
-    dataset = load_dataset("parquet", data_files={'train': 'data/data_cleaned_sentences_phases_rules.parquet'})
-    df = dataset['train'].to_pandas()
-    processed_df = process_dataframe(df)
-    aggregated_df = aggregate_and_filter_data(processed_df)
-    save_data(aggregated_df)
+    df_v1 = aggregated_df[filter_1]
+    df_v1.to_csv('data/raw_data_filtered_v1.csv', index=False)
+    df_v2 = aggregated_df[filter_2]
+    df_v2.to_csv('data/raw_data_filtered_v2.csv', index=False)
+    train_val, test = train_test_split(df_v2, test_size=0.10, random_state=42)
+    train, val = train_test_split(train_val, test_size=(1/9), random_state=42) 
+    pd.concat([train.assign(split='train'), val.assign(split='val'), test.assign(split='test')]).to_csv('data/TurkishHatePrintCorpus.csv', index=False)
 
     pandarallel.initialize(progress_bar=True)
-    sample_df = load_sample_data()
+    sample_df = df.sample(frac=0.01)
     corpus = ' '.join(sample_df['content'].tolist())
     sentences = extractor.from_paragraph(corpus)
     print('Number of sample sentences', len(sentences))
