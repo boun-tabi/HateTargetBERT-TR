f0b1426475da2d1edf08bdf9cc826a53378fb2e4
Gökçe Uludoğan <gokceuludogan@gmail.com>
2023-09-17 19:57:06 +0300

    Fix test output paths

--- src/test.py
+++ src/test.py
@@ -28,8 +28,6 @@ checkpoint = torch.load(args.load_from, map_location=f'cuda:{gpu_id}')
 config_file = checkpoint['config_file']
 args = dotdict(json.load(open(config_file, 'r')))
 
-is_multigpu = "0" in args.gpu_ids and "1" in args.gpu_ids
-
 if args.model_type == "HateTargetNN":
     tokenizer = None
     model = HateTargetNN(num_labels=args.num_classes, rule_dimension=26)
@@ -38,6 +36,7 @@ else:
     tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-128k-uncased")
     model = HateTargetBERT(checkpoint="dbmdz/bert-base-turkish-128k-uncased", num_labels=args.num_classes, rule_dimension=26)
 
+only_rules = args.model_type == "HateTargetNN"
 test_dataset = HateSpeechDataset(split="test", 
                                  tokenizer=tokenizer, 
                                  data_path=args.dataset_dir, 
@@ -48,9 +47,7 @@ test_dataset = HateSpeechDataset(split="test",
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=True)
 
 
-device = f'cuda:{args.gpu_ids}' if not is_multigpu else 'cuda:0'
-if is_multigpu:
-    model = nn.DataParallel(model)
+device = f'cuda:{args.gpu_id}' if torch.cuda.is_available() else 'cpu'
 model.to(device)
 model.load_state_dict(checkpoint['model_state_dict'])
 
@@ -58,9 +55,9 @@ criterion = nn.CrossEntropyLoss()
 test_metrics = evaluate_model(model, test_loader, criterion, device, args)
 
 print(test_metrics)
-with open(os.path.join("checkpoints/hatetargetbert", "test_metrics.json"), "w") as f:
+with open(os.path.join(args.load_from, "test_metrics.json"), "w") as f:
     json.dump(test_metrics, f)
 
 all_predictions = [output.argmax(dim=1).cpu().numpy() for output in test_metrics['outputs']]
 pred_df = test_dataset._get_prediction_results(all_predictions)
-pred_df.to_csv(os.path.join("checkpoints/hatetargetbert", "test_predictions.csv"), index=False)
+pred_df.to_csv(os.path.join(args.load_from, "test_predictions.csv"), index=False)
